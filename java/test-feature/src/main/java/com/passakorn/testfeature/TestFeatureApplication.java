205023199f0ce755fb45741ae683f754e2a80644
passakornsdev@gmail.com <passakornsdev@gmail.com>
2023-01-10 21:41:43 +0700

    initial spring boot source

---
+++ java/test-feature/src/main/java/com/passakorn/testfeature/TestFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.passakorn.testfeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFeatureApplication.class, args);
+	}
+
+}
---
+++ java/test-feature/src/test/java/com/passakorn/testfeature/TestFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passakorn.testfeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
